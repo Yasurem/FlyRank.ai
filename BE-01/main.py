e9f18567cb6ad58e5ea80e4d9d5ffdc154363d16
Yasurem <jmcas88@gmail.com>
2026-07-18 01:46:54 +0800

    Stage 3: create with validation

--- BE-01/main.py
+++ BE-01/main.py
@@ -5,9 +5,20 @@ app = FastAPI()
 
 # Dict of tasks
 tasks = {
-    1: {"id": 1, "title": "Create a simple CRUD app", "completed": False },
-    2: {"id": 2, "title": "Finish Linear Regression Lecture", "completed": True },
-    3: {"id": 3, "title": "Review basic probability concepts and Bayes' Theorem", "completed": True }
+    1: {"id": 1, 
+        "title": "Create a simple CRUD app", 
+        "done": False 
+    },
+
+    2: {"id": 2, 
+        "title": "Finish Linear Regression Lecture", 
+        "done": True 
+    },
+
+    3: {"id": 3, 
+        "title": "Review basic probability concepts and Bayes' Theorem", 
+        "done": True 
+    }
 }
 
 # Endpoints
@@ -28,7 +39,53 @@ async def get_tasks():
 async def get_task(id: int):
     if id in tasks:
         return tasks[id]
+    
     return JSONResponse(
             status_code=404,
             content={"error": f"Task {id} not found"}
         )
+
+# Stage 3
+@app.post("/tasks")
+async def create_task(task: dict):
+
+    # Validate input
+    if "title" not in task:
+        return JSONResponse(
+            status_code=400,
+            content={"error": "Task title is missing"}
+        )
+
+    # Ensure the title is a string
+    if not isinstance(task["title"], str):
+        return JSONResponse(
+            status_code=400,
+            content={"error": "Task title must be a string"}
+        )
+
+    # Validate cleaned title
+    task["title"] = task["title"].strip()
+
+    if task["title"] == "":
+        return JSONResponse(
+            status_code=400,
+            content={"error": "Task title is empty"}
+        )
+
+    # Assign a new ID to the task
+    new_id = max(tasks.keys())+ 1 if tasks else 1
+
+    # Assign the new ID to the task and set done to default False
+    new_task = {
+        "id": new_id,
+        "title": task["title"],
+        "done": False
+    }
+
+    # Add it to the tasks dictionary
+    tasks[new_id] = new_task
+
+    return JSONResponse(
+        status_code=201,
+        content={"message": f"Done, here's your receipt.", "task": new_task}
+    )
