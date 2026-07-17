853690be03fac9259d10400f4a2e6057468d9091
Yasurem <jmcas88@gmail.com>
2026-07-18 00:45:15 +0800

    Stage 2: read endpoints with 404

--- BE-01/main.py
+++ BE-01/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
+# Dict of tasks
+tasks = {
+    1: {"id": 1, "title": "Create a simple CRUD app", "completed": False },
+    2: {"id": 2, "title": "Finish Linear Regression Lecture", "completed": True },
+    3: {"id": 3, "title": "Review basic probability concepts and Bayes' Theorem", "completed": True }
+}
+
+# Endpoints
 @app.get("/")
 async def root():
     return { "name": "Task API", "version": "1.0", "endpoints": ["/tasks"] }
@@ -9,3 +18,17 @@ async def root():
 @app.get("/health")
 async def health():
     return { "status": "ok" }
+
+# Stage 2
+@app.get("/tasks")
+async def get_tasks():
+    return tasks
+
+@app.get("/tasks/{id}")
+async def get_task(id: int):
+    if id in tasks:
+        return tasks[id]
+    return JSONResponse(
+            status_code=404,
+            content={"error": f"Task {id} not found"}
+        )
